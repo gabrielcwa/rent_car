9a7975d4e73936b5c8e87fedea7713ff4c5d665c
Gabriel Cavalcanti <gabriel@lainformatica.com.br>
2010-11-07 15:36:51 -0300

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentCar::Application.config.secret_token = '29af8f6cd2ef4c1ceae2049a3af0385770047c5b5d1320711fb5b1ffb449e8f3f06cc1a5d783459669b1a43bd4ba620ef67e677809f5acfed0cc3ccacaa8b10a'
