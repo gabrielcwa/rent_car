f322ef2a2c35b2900ae42b9fccd5599f3be529f3
Gabriel Cavalcanti <gabriel@lainformatica.com.br>
2010-11-09 00:02:58 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ RentCar::Application.routes.draw do
 
   resources :clientes
   
-  root to: "clientes#index"
+  # root to: "clientes#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
